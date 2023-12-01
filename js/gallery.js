2ca5c3ac2246b4bcc11807659a2f42dc4864c5e4
Abhinav Majeti <abhimajeti24@gmail.com>
2023-12-01 13:06:13 +0530

    Added styling to close button and modified navbar button

--- js/gallery.js
+++ js/gallery.js
@@ -10,16 +10,16 @@ images.forEach((img) => {
 let imgModal = (src) => {
     const modal = document.createElement("div");
     modal.setAttribute("class", "modal");
-    //add the modal to the main section or the parent element
+
     document.querySelector("html").append(modal);
-    //adding image to modal
+
     const newImage = document.createElement("img");
     newImage.setAttribute("src", src);
     modal.append(newImage)
-    //creating the close button
+
     const closeBtn = document.createElement("div");
     closeBtn.setAttribute("class", "closeBtn");
-    //close function
+
     modal.append(newImage, closeBtn);
     document.querySelector(".closeBtn").innerHTML = "&times;";
     modal.onclick = () => {
